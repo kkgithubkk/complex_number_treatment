540277d99cfa00ace22bb80e96f5810c9b89d46e
kensuke.konishi <kensuke.konishi@systemi.co.jp>
2023-11-15 20:15:06 +0900

    feat: Java

---
+++ java/Main.java
@@ -0,0 +1,30 @@
+import org.apache.commons.math3.complex.Complex;
+
+public class Main {
+    public static void main(String[] args) {
+//        Java 20.0.1 commons-math3 3.6.1
+// Complex 2 + 3i => a
+        Complex a = Complex.valueOf(2, 3);
+        System.out.println("a=" + a);
+
+// Complex 4 - 2i => b
+        Complex b = Complex.valueOf(4, -2);
+        System.out.println("b=" + b);
+
+// Real part of 'a'
+        double realPartA = a.getReal();
+        System.out.println("Real part of a is " + realPartA);
+
+// Imaginary part of 'a'
+        double imaginary_part_a = a.getImaginary();
+        System.out.println("Imaginary part of a is " + imaginary_part_a);
+
+// Conjugate of a
+        Complex conj_a = a.conjugate();
+        System.out.println("Conjugate of a is "+conj_a);
+
+// |a|
+        double abs_a = a.abs();
+        System.out.println("Absolute value of a is " + abs_a);
+    }
+}
